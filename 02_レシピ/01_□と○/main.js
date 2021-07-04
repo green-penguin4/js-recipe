6c957ba7a3be27e00c9ca5c001dd1988f9131acc
green-penguin4 <unemotom2021@gmail.com>
2021-07-04 17:11:13 +0900

    [ストップウォッチ]を学習しました。

--- 02_レシピ/01_□と○/main.js
+++ 02_レシピ/01_□と○/main.js
@@ -1,5 +1,6 @@
 const figure = document.getElementById("figure")
 const figureTwo = document.getElementById("figure_2")
+const figureThree = document.getElementById("figure_3")
 
 figure.onclick = function() {
   figure.classList.toggle("rounded")
