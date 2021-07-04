46d655a111aa8fa2b860ccaacbc4806232eb3634
green-penguin4 <unemotom2021@gmail.com>
2021-07-04 16:52:35 +0900

    [と発展１]を学習しました。

--- 02_レシピ/01_□と○/main.js
+++ 02_レシピ/01_□と○/main.js
@@ -1,5 +1,10 @@
 const figure = document.getElementById("figure")
+const figureTwo = document.getElementById("figure_2")
 
 figure.onclick = function() {
   figure.classList.toggle("rounded")
+}
+
+figureTwo.ondblclick = function() {
+  figureTwo.classList.toggle("rounded")
 }
